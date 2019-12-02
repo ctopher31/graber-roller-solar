83f5345de91f95d6ed4e9d2f002f67e749663ca1
Christopher Arritt <ctopher31@yahoo.com>
2019-12-01 22:02:19 -0600

    Add filtering of collections

--- src/js/swatches.js
+++ src/js/swatches.js
@@ -4,6 +4,8 @@ import { getSwatches, buildSwatch, buildBlurb, swatchClickHandler } from './modu
 const path = '../assets/images/swatches';
 const swatchGallery = document.getElementById('swatches');
 
+export const filterItems = (filterDeck, items) => items.reduce((accum, item) => [...accum, ...Object.keys(item).filter(key => filterDeck.every(filterItem => item[key] === filterItem))], []);
+
 export default () => {
   getSwatches('../assets/js/graber-roller-solar-swatches.json')
     .then((swatches) => {
@@ -23,6 +25,23 @@ export default () => {
       }, 50);
 
       document.addEventListener('click', swatchClickHandler, false);
+      let filterDeck = [];
+      const filters = document.querySelectorAll('.filter');
+      console.log(filters);
+      [].map.call(filters, filter => filter.addEventListener('change', () => {
+        console.log('Change');
+        [].map.call(swatchGallery.children, () => {
+          while (swatchGallery.firstElementChild) {
+            swatchGallery.removeChild(swatchGallery.firstElementChild);
+          }
+        });
+        // filterDeck = [...[].filter.call(filters, filterItem => (filterItem.selected ? filterItem.getAttribute('data-value') : false))];
+        // const items = filterItems(filterDeck, swatches);
+        // items.map((swatch) => {
+        //   const swatchContainer = buildSwatch(swatch, `${path}/300x300`, `${path}/600x600`);
+        //   swatchGallery.appendChild(swatchContainer);
+        // });
+      }, false));
     })
     .catch(err => new Error(err));
 };
